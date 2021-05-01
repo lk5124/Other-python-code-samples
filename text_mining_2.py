3fb65597965bb3222f3884fbb48f5267c76ca0d4
Artiom Chaustov <70112351+lk5124@users.noreply.github.com>
2021-05-02 00:28:05 +0300

    Update text_mining_2.py

--- text_mining_2.py
+++ text_mining_2.py
@@ -6,11 +6,11 @@ import numpy as np
 
 
 
-# If you would like to work with the raw text you can use 'moby_raw'
+
 with open('moby.txt', 'r') as f:
     moby_raw = f.read()
 
-# If you would like to work with the novel in nltk.Text format you can use 'text1'
+
 moby_tokens = nltk.word_tokenize(moby_raw)
 text1 = nltk.Text(moby_tokens)
 
